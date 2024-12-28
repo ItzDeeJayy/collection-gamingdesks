2d85da9f27b04f4ddac872766eed13a014af156f
DJ <136165865+ItzDeeJayy@users.noreply.github.com>
2024-12-28 15:58:13 -0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+-- FX Information --
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
+
+-- Resource Information --
+name         'Collection Gaming Desks Bundle Pack'
+version      '1.0.0'
+author       'DJ'
+description  'Bundle Pack of Gaming Desks Bundle Pack'
+
+-- Manifest --
+dependencies {
+    '/server:5181',
+    '/onesync'
+  }
+
+data_file 'DLC_ITYP_REQUEST' 'stream/props.ytyp'
+
+dependency '/assetpacks'
